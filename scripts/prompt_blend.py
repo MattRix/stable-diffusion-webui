27485000a2a5c181bce09229ce656da97dcec806
Matt Rix <matt@matt-rix.com>
2022-09-13 17:38:44 -0400

    Cleaning up prompt blend

--- scripts/prompt_blend.py
+++ scripts/prompt_blend.py
@@ -2,11 +2,7 @@ import modules.scripts as scripts
 import gradio as gr
 import torch
 
-from modules.processing import Processed, fix_seed, process_images
-from modules import processing, shared, sd_samplers
-from modules.sd_samplers import samplers
-from modules.shared import opts, cmd_opts, state
-
+from modules.processing import fix_seed, process_images
 
 class Script(scripts.Script):
 
@@ -15,11 +11,8 @@ class Script(scripts.Script):
 
     def show(self, is_img2img):
         return True
-        #return not is_img2img
 
     def ui(self, is_img2img):
-           
-        #how_to = gr.HTML(value="<div style='margin:15px'>Note: Use a higher batch count for more frames. All images use the same seed (even in batches as long as you use a non-'a' sampler).</div>")
         start_prompt = gr.Textbox(label="Start prompt", lines=1)
         end_prompt = gr.Textbox(label="End prompt", lines=1)
             
@@ -28,10 +21,11 @@ class Script(scripts.Script):
             end_percent = gr.Number(label="End percent", value=1.0)
             prompt_usage = gr.Dropdown(label="Main prompt", choices=["Ignore","Prefix","Suffix"], value="Ignore")
 
-        return [start_prompt, end_prompt, start_percent, end_percent, prompt_usage]
+        notes = gr.HTML(value="<div style='margin:15px'>Note: Use a higher batch count for more frames. All images use the same seed (even in batches as long as you use a non-'a' sampler).</div>")
 
+        return [start_prompt, end_prompt, start_percent, end_percent, prompt_usage, notes]
 
-    def run(self, p, start_prompt, end_prompt, start_percent, end_percent, prompt_usage):
+    def run(self, p, start_prompt, end_prompt, start_percent, end_percent, prompt_usage, notes):
 
         if prompt_usage == "Prefix":
             start_prompt = f"{p.prompt} {start_prompt}"
@@ -41,9 +35,8 @@ class Script(scripts.Script):
             start_prompt = f"{start_prompt} {p.prompt}"
             end_prompt = f"{end_prompt} {p.prompt}"
 
-
         self.total = p.n_iter*p.batch_size 
-        self.i = 0
+        self.iteration = 0
 
         self.original_sample = p.sample
 
@@ -55,29 +48,20 @@ class Script(scripts.Script):
             conds = []
 
             for _ in range(p.batch_size):
+                blend_percent = self.iteration/(self.total-1) if self.total > 1 else 0.5 #if we are only generating one image, create a 50% blend between start and end prompt 
+                blend_percent = start_percent + blend_percent * (end_percent-start_percent) #remap percent to within a specific range
+                conds.append(torch.lerp(start_cond,end_cond,blend_percent)) #blend/lerp between the actual conditioning tensors 
 
-                #if we are only generating one image, create a 50% blend between start and end prompt
-                blend_percent = self.i/(self.total-1) if self.total > 1 else 0.5 
-
-                #remap percent to within a specific range
-                blend_percent = start_percent + blend_percent * (end_percent-start_percent) 
-
-                #blend/lerp between the actual conditioning tensors 
-                conds.append(torch.lerp(start_cond,end_cond,blend_percent))
-
-                #we want to blend smoothly within the batches
-                self.i += 1 
+                self.iteration += 1 #we want to blend smoothly within the batches 
 
             conditioning = torch.cat(conds)
 
-            #sampler = samplers[p.sampler_index].constructor(p.sd_model)
-            #samples_ddim = sampler.sample(p, x, conditioning, unconditional_conditioning)
             samples_ddim = self.original_sample(x, conditioning, unconditional_conditioning)
             
             return samples_ddim
 
         p.sample = sample_extra
-        
+    
         p.prompt = f"{start_prompt} to {end_prompt}"
 
         fix_seed(p) #use the specified seed or get a random one if needed
