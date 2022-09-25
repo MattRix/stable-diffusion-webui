45f9952b58d8345d6f30bd014cf66d478d97f44f
Matt Rix <matt@matt-rix.com>
2022-09-25 15:01:34 -0400

    Make video halftime etc.

--- animscripts/get_frames.bat
+++ animscripts/get_frames.bat
@@ -1,4 +1,4 @@
 rmdir "frames" /s /q
 mkdir "frames"
 ::actual headphones frame rate is 25
-ffmpeg -ss 10 -i "headphones.mp4" -t 5 -s 960x512 -r 25 "in_images/%%04d.png" 
+ffmpeg -ss 6 -i "Thinking-82130.mp4" -t 10 -s 960x512 -r 12 "in_images_thinking/%%04d.png" 
--- animscripts/make_video.bat
+++ animscripts/make_video.bat
@@ -7,10 +7,7 @@
 set /p FOLDERID="Folder id: "
 
 ::regular mp4
-ffmpeg -r 25 -f image2 -s 640x384 -i "out_images_%FOLDERID%/%%04d.png" -vcodec libx264 -crf 2  -pix_fmt yuv420p "results/headphones_%FOLDERID%.mp4"
+ffmpeg -r 25 -f image2 -s 640x384 -i "out_images_%FOLDERID%/%%04d.png" -vcodec libx264 -crf 2  -pix_fmt yuv420p "results/thinking_%FOLDERID%.mp4"
 
 ::shareable size gif
-ffmpeg -r 25 -f image2 -s 640x384 -i "out_images_%FOLDERID%/%%04d.png" -vf "fps=25,scale=640:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=256:reserve_transparent=0[p];[s1][p]paletteuse" "results/headphones_%FOLDERID%.gif"
-
-::source gif
-::ffmpeg -r 25 -f image2 -s 640x380 -i "in_images_%FOLDERID%/%%04d.png" -vf "fps=25,scale=640:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=256:reserve_transparent=0[p];[s1][p]paletteuse" "results/headphones_source.gif"
+ffmpeg -r 25 -f image2 -s 640x384 -i "out_images_%FOLDERID%/%%04d.png" -vf "fps=25,scale=640:-2:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=256:reserve_transparent=0[p];[s1][p]paletteuse" "results/thinking_%FOLDERID%.gif"
